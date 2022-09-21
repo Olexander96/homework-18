b6118f578241969b2a1d2fe8982ad026f3edc0ba
Olexander96 <o.sheiko96@gmail.com>
2022-09-21 15:57:51 +0300

    fixed after lesson

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// 1) 
+//Кожну функцію потрібно визивати окремо, так як кожна змінює масив!
+
+//Мінімум
+// 1) Створи масив «Список покупок». Кожен елемент масиву є об'єктом, який містить назву продукту, 
+    //кількість і куплений він чи ні, ціну за одиницю товару, сума. Написати кілька функцій для роботи з таким масивом:
     const productList = [
         {productName: 'potatoe', number: 5, isBuy: true, priseForOne: 2, totalSum: 10},
         {productName: 'wine', number: 2, isBuy: false, priseForOne: 7, totalSum: 14},
@@ -6,7 +10,8 @@
         {productName: 'milk', number: 1, isBuy: false, priseForOne: 5, totalSum: 5}
     ];
 
-        // 1.1)
+        // 1.1) Виводити весь список на екран таким чином, щоб спочатку йшли продукти, що ще не придбані, 
+        //а потім - ті, що вже придбали.
         function showList(array) {
             array.sort(function(a, b) {
                 if (a.isBuy == true && b.isBuy == false) {
@@ -20,7 +25,7 @@
             return array;
         };
 
-        // 1.2)
+        // 1.2) Покупка продукту. Функція приймає назву продукту і відзначає його як придбаний.
         function isBought(name) {
             for (let key of productList) {
                 if (key.productName == name) {
@@ -30,7 +35,9 @@
             return productList;
         };
 
-    // 2)
+//Норма
+// 2) Видалення продукту зі списку (видалення повинно проводитися шляхом створення нового масиву, в якому продукт, 
+    // що ми шукаємо, буде відсутнім)
     function deleteProduct(name) {
         for (let key of productList) {
             if (key.productName == name) {
@@ -40,7 +47,9 @@
         return productList;
     };
 
-    // 3)
+// 3) Додавання покупки в список. Враховуй, що при додаванні покупки з уже існуючим в списку продуктом, 
+    //необхідно збільшувати кількість в існуючій покупці, а не додавати нову. При цьому також повинна змінитися сума, 
+    //наприклад, якщо ціна за одиницю 12, а кількості товарів стало 2, то сума буде 24.
     function addProduct(name, num, bought, prise) {
         let total = num * prise;
         const newProduct = {
@@ -54,7 +63,8 @@
         return productList;
     };
 
-    // 4)
+//Максимум
+// 4)Підрахунок суми всіх продуктів (враховуючи кількість кожного) в списку.
     function totalCost(array) {
         let cost = 0;
         for (let key of array) {
@@ -63,7 +73,7 @@
         return `Сума всіх продуктів  = ${cost}`
     };
 
-    // 5)
+// 5) Підрахунок суми всіх (не) придбаних продуктів.
     function totalForNotBought(array) {
         let cost = 0;
         for (let key of array) {
@@ -74,7 +84,8 @@
         return `Сума некуплених продуктів  = ${cost}`
     };
 
-    // 6)
+// 6) Показання продуктів в залежності від суми, (від більшого до меншого / від меншого до більшого, в залежності 
+    //від параметра функції, який вона приймає)
     function listFromRrices(array) {
         array.sort(function(a, b) {
             if (a.totalSum < b.totalSum) {
